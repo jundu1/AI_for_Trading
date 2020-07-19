abc2a497509452b3c81a131f5216b1b120e200c7
dujun1 <dujun.john@gmail.com>
2020-07-19 15:32:45 -0400

    model_evaluation exercise

---
+++ exercise/model_evaluation/learning_curve/draw_larning_curve.py
@@ -0,0 +1,26 @@
+# Import, read, and split data
+import pandas as pd
+data = pd.read_csv('data.csv')
+import numpy as np
+X = np.array(data[['x1', 'x2']])
+y = np.array(data['y'])
+
+# Fix random seed
+np.random.seed(55)
+
+### Imports
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+
+# TODO: Uncomment one of the three classifiers, and hit "Test Run"
+# to see the learning curve. Use these to answer the quiz below.
+
+### Logistic Regression
+estimator = LogisticRegression()
+
+### Decision Tree
+estimator = GradientBoostingClassifier()
+
+### Support Vector Machine
+estimator = SVC(kernel='rbf', gamma=1000)
